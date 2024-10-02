4651fe573d02f5bb0be068e032d8417605e5b0f2
Luis Vazquez <sur97c@gmail.com>
2024-10-01 20:59:39 -0600

    Integrate Header component into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,10 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import { Inter } from 'next/font/google'
 import "@styles/globals.scss"
 import '@fortawesome/fontawesome-svg-core/styles.css'
-// import { AuthStateListener } from "@components/auth-state-listener/AuthStateListener"
-// import { FlipProvider } from "@providers/flip-provider"
-// import { RootState, useAppSelector } from "@lib/redux/store"
 import { ClientProviders } from "@providers/client-provider"
 import { AuthStateListener } from "@components/auth-state-listener/AuthStateListener"
 import { FlipProvider } from "@providers/flip-provider"
 import Header from '@components/header/Header'
-// import Navigation from "@components/navigation/Navigation"
 
 config.autoAddCss = false;
 
@@ -33,8 +29,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const { isAuthenticated } = useAppSelector((state: RootState) => state.auth);
-
   return (
     <html lang="es" className={`${inter.variable} font-sans`}>
       <body>
@@ -42,13 +36,8 @@ export default function RootLayout({
           <AuthStateListener>
             <div>
               <FlipProvider>
-                <Header
-                  setIsLoginOpen={setIsLoginOpen}
-                  isLoginOpen={isLoginOpen}
-                  title={activePage.title} />
+                <Header />
               </FlipProvider>
-              {/* 
-              {isAuthenticated && <Navigation />} */}
             </div>
           </AuthStateListener>
           <main className="container mx-auto mt-4">
