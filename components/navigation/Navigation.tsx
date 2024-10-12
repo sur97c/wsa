11cfddeca06494636c96ad892e4d6e889f817f51
Luis Vazquez <sur97c@gmail.com>
2024-10-12 09:34:19 -0600

    Refactor to Navigation component to support languages and new Roles structure

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,48 +1,50 @@
 // components/navigation/Navigation.tsx
+
 'use client'
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { RootState, useAppSelector } from "@lib/redux/store";
-import { Roles } from '@models/Roles';
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { RootState, useAppSelector } from "@lib/redux/store"
+import { useTranslations } from '@hooks/useTranslations'
+import { useRoles } from '@hooks/useRoles'
+import { useLanguage } from '@hooks/useLanguage'
+import { IUserClaims } from '@models/IUserClaims'
+import { RoleKey } from '@utils/rolesDefinition'
 
 export default function Navigation() {
-    const [isOpen, setIsOpen] = useState(false);
-    const pathname = usePathname();
-    const auth = useAppSelector((state: RootState) => state.auth.auth);
-    // const userRoles = user?.profile?.customClaims?.roles ? 
-    //     ((user.profile.customClaims.roles as unknown) as string).split(',') : 
-    //     [];
-    const userRoles = (auth?.customClaims?.roles as string)
-    // userRoles ?
-    //     (userRoles as string).split(',') :
-    //     [];
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+    const { t, translations } = useTranslations()
+    const Roles = useRoles()
+    const [isOpen, setIsOpen] = useState(false)
+    const pathname = usePathname()
+    const auth = useAppSelector((state: RootState) => state.auth.auth)
+    const userClaims = auth?.customClaims as IUserClaims | undefined
+    const userRoles = userClaims?.roles || []
+    const toggleMenu = () => setIsOpen(!isOpen)
+    const { language } = useLanguage()
 
     const filteredRoles = Roles.filter(role =>
         auth?.isAuthenticated && (
-            userRoles?.includes(role.key) || role.key === 'home'
+            userRoles.includes(role.key as RoleKey) || role.key === 'home'
         )
-    );
+    )
 
     return (
         <nav className="bg-secondary-light bg-opacity-80 p-4 shadow-lg">
             <div className="container mx-auto flex justify-between items-center">
-                <div className="text-dark text-lg font-bold hover:text-primary-hover">
-                    <Link href="/">WSA Broker</Link>
+                <div className="text-dark text-lg font-bold hover:text-primary-hover hover:font-bold">
+                    <Link href={`/${language}`}>{t(translations.navigation.title)}</Link>
                 </div>
                 <div className="hidden md:flex space-x-4">
                     {filteredRoles.map((role) => (
                         <Link
                             key={role.key}
-                            href={`/${role.key}`}
-                            className={`text-dark hover:text-primary-hover ${pathname === `/${role.key}` ? 'text-primary font-extrabold' : ''
+                            href={role.key === 'home' ? `/${language}` : `/${language}/${role.key}`}
+                            className={`text-dark hover:text-primary-hover hover:font-bold ${pathname === (role.key === 'home' ? `/${language}` : `/${language}/${role.key}`)
+                                ? 'text-primary font-extrabold'
+                                : ''
                                 }`}
                         >
                             {role.menuLabel}
@@ -60,8 +62,10 @@ export default function Navigation() {
                     {filteredRoles.map((role) => (
                         <Link
                             key={role.key}
-                            href={`/${role.key}`}
-                            className={`block py-2 text-dark hover:text-primary-hover ${pathname === `/${role.key}` ? 'text-primary font-extrabold' : ''
+                            href={role.key === 'home' ? `/${language}` : `/${language}/${role.key}`}
+                            className={`block py-2 text-dark hover:text-primary-hover hover:font-bold ${pathname === (role.key === 'home' ? `/${language}` : `/${language}/${role.key}`)
+                                ? 'text-primary font-extrabold'
+                                : ''
                                 }`}
                             onClick={() => setIsOpen(false)}
                         >
@@ -71,5 +75,5 @@ export default function Navigation() {
                 </div>
             )}
         </nav>
-    );
+    )
 }
